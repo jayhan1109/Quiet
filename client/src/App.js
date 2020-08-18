1cad91673c883f246092c2d2084d70d982bdfe98
jayhan1109 <jayhan1109@gmail.com>
2020-08-19 02:13:59 +0900

    Setup Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Navbar from "./components/navbar/Navbar";
+import { Route, Switch } from "react-router-dom";
+import Landing from "./components/landing/Landing";
 
 const App = () => {
-  return <div>Quiet</div>;
+  return (
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+      </Switch>
+    </Fragment>
+  );
 };
 
 export default App;
---
+++ client/src/components/landing/Landing.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Landing = () => {
+  return (
+    <div>
+      <h1>Landing Body</h1>
+    </div>
+  );
+};
+
+export default Landing;
